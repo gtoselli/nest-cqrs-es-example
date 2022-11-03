3d10544b8edd7f7cab23c6862cd53beeceba1f57
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-11-03 16:46:39 +0100

    feat(infra): add aggregate and event

---
+++ src/infra/event.ts
@@ -0,0 +1,26 @@
+export abstract class Event<EventPayloadType> {
+  abstract readonly eventName: string;
+  abstract readonly aggregateId: string;
+  abstract readonly eventPayload: EventPayloadType;
+  abstract readonly isPublic: boolean;
+}
+
+export class PrivateEvent<EventPayloadType> implements Event<EventPayloadType> {
+  readonly isPublic = false;
+
+  constructor(
+    readonly eventName: string,
+    readonly aggregateId: string,
+    readonly eventPayload: EventPayloadType,
+  ) {}
+}
+
+export class PublicEvent<EventPayloadType> implements Event<EventPayloadType> {
+  readonly isPublic = true;
+
+  constructor(
+    readonly eventName: string,
+    readonly aggregateId: string,
+    readonly eventPayload: EventPayloadType,
+  ) {}
+}
