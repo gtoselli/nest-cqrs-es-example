1680bce54f55a8965180be704d686e1d61b30362
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-11-03 16:55:54 +0100

    feat(cart): add creation by createEmpty factory

--- src/cart/domain/Cart.aggregate.spec.ts
+++ src/cart/domain/Cart.aggregate.spec.ts
@@ -1,19 +1,23 @@
 import { CartAggregate } from './Cart.aggregate';
+import { validate as isValidUUID } from 'uuid';
 
 describe('Cart aggregate', () => {
-  let cart: CartAggregate;
-
   const cartIdFixture = 'foo-cart-id';
 
-  beforeEach(() => {
-    cart = new CartAggregate(cartIdFixture);
-  });
-
   it('should be defined', () => {
+    const cart = new CartAggregate(cartIdFixture);
     expect(cart).toBeDefined();
   });
 
   it('should be an aggregate root extension', () => {
+    const cart = new CartAggregate(cartIdFixture);
     expect(cart.id).toBeDefined();
+  });
+
+  describe('Cart creation', () => {
+    it('should create an empty cart with a uuid', () => {
+      const cart = CartAggregate.createEmpty();
+      expect(isValidUUID(cart.id)).toBeTruthy();
+    });
   });
 });
--- src/cart/domain/Cart.aggregate.ts
+++ src/cart/domain/Cart.aggregate.ts
@@ -1,7 +1,13 @@
 import { AggregateRoot } from '../../infra/aggregate';
+import { v4 as uuid } from 'uuid';
 
 export class CartAggregate extends AggregateRoot {
   constructor(cartId: string) {
     super(cartId);
+  }
+
+  public static createEmpty() {
+    const cartId = uuid();
+    return new CartAggregate(cartId);
   }
 }
