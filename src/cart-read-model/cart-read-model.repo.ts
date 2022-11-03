1dfb5c45054ff7a6d289a9bc4952735dfcfa6eea
Gabriele Toselli <toselli.gabriele@gmail.com>
2022-11-03 22:40:16 +0100

    feat(cart-read-model): add fake persistence with node-persist

--- src/cart-read-model/cart-read-model.repo.ts
+++ src/cart-read-model/cart-read-model.repo.ts
@@ -1,29 +1,42 @@
+import { OnModuleInit } from '@nestjs/common';
+import * as fakeLocalDb from 'node-persist';
+
 export type CartReadModel = {
     cartId: string;
     itemsCount: number;
     isDeleted: boolean;
 };
 
-export class CartReadModelRepo {
-    constructor(private carts: CartReadModel[] = []) {}
+const fakeLocalDbCartsKey = 'carts-read-model';
+
+export class CartReadModelRepo implements OnModuleInit {
+    async onModuleInit() {
+        await fakeLocalDb.init({});
+    }
 
     public async getOne(cartId: string): Promise<CartReadModel | null> {
-        return this.carts.find((c) => c.cartId === cartId);
+        const carts = (await fakeLocalDb.get(fakeLocalDbCartsKey)) || [];
+        return carts.find((c) => c.cartId === cartId);
     }
 
     public async createOne(cartReadModel: CartReadModel): Promise<void> {
-        this.carts.push(cartReadModel);
+        const carts = (await fakeLocalDb.get(fakeLocalDbCartsKey)) || [];
+        carts.push(cartReadModel);
+        await fakeLocalDb.set(fakeLocalDbCartsKey, carts);
     }
 
     public async updateOne(cartId: string, stuffToUpdate: Partial<CartReadModel>): Promise<void> {
-        const cartIndex = this.carts.findIndex((c) => c.cartId === cartId);
+        const carts = (await fakeLocalDb.get(fakeLocalDbCartsKey)) || [];
+
+        const cartIndex = carts.findIndex((c) => c.cartId === cartId);
         if (cartIndex === -1) throw new Error('Cart to update not found');
 
-        this.carts[cartIndex] = { ...this.carts[cartIndex], ...stuffToUpdate };
+        carts[cartIndex] = { ...carts[cartIndex], ...stuffToUpdate };
+        await fakeLocalDb.set(fakeLocalDbCartsKey, carts);
         return;
     }
 
     public async getMany(): Promise<CartReadModel[]> {
-        return this.carts;
+        return (await fakeLocalDb.get(fakeLocalDbCartsKey)) || [];
     }
 }
